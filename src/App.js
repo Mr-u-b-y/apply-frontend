4dffce08b7a2b4d362f6ace89966994f7c1b18b8
muneeba <muneebaruby6236@gmail.com>
2022-10-27 14:03:24 +0530

    Navbar & AddEmployee UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Navbar />
+    <AddEmployee/>
+    </>
   );
 }
 
